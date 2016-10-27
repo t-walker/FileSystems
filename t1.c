36388334bb37ec9205831acca05c269502f2f239
twalker <twalker@eecs.wsu.edu>
2016-10-26 19:34:38 -0700

    Update t1.c

--- t1.c
+++ t1.c
@@ -24,7 +24,46 @@ char path[200], buf[1024], *deviceName = "disk";
 
 MINODE *iget(int dev, int ino)
 {
+    MINODE *mip = malloc(sizeof(MINODE));
+    int i,
+        blk_num,
+        offset;
 
+    //Loop through all of the minodes to see if the one we want is in memory
+    for (i = 0; i < NMINODE; i++) {
+            if (minode[i].ino == ino) {     //Do the values match
+                minode[i].refCount++;
+                return &minode[i];
+            }
+    }
+
+    //Use mailman's to get the block location
+    blk_num = ((ino - 1)/8) + inode_start;
+    offset = (ino - 1) % 8;
+    //Get the block
+    get_block(dev, blk_num, buf);
+    ip = (INODE *)buf + offset;
+
+    //Loop through all of the minodes until we find one that is empty
+    for (i = 0; i < NMINODE; i++){
+        if (minode[i].refCount == 0){           //Check to make sure it's empty
+            /***
+             * 1. Set the minode's inode to the temorary inode
+             * 2. Set the dirty value to 0
+             * 3. Set the device number (works like a fd)
+             * 4. Set the inode number
+             * 5. Increment the refcount
+             ***/
+            minode[i].INODE = *ip;
+            minode[i].dirty = 0;
+            minode[i].dev = dev;
+            minode[i].ino = ino;
+            minode[i].refCount = 1;
+
+            //Return the index of the inode
+            return &minode[i];
+        }
+    }
 }
 
 void iput(MINODE *mip)
