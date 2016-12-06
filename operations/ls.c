ff64a8afe2d5ce5b24fc1b976792d7463449706e
Tyler Walker <twalker@eecs.wsu.edu>
2016-12-05 21:23:43 -0800

    Moving printDir into the ls folder

--- operations/ls.c
+++ operations/ls.c
@@ -55,3 +55,29 @@ void ls (char *pathname)
         }
 
 }
+
+void printDir(INODE ptr, int dev)
+{
+        if((ptr.i_mode & 0x4000) == 0x4000)
+        {
+                time_t t;
+                //printf("printDir()\n");
+                char buf[1024];
+                char *cp;
+                get_block(dev, ptr.i_block[0],buf);
+                dp = (DIR *)buf;
+                cp = buf;
+
+                while(cp < buf + 1024) {
+                        t = ptr.i_mtime;
+                        printf("%6o %4d  %4d  %4d  %15s \t %20s", ptr.i_mode, dp->inode, dp->rec_len, dp->name_len, dp->name, ctime(&t)); //prints out traversal
+                        cp += dp->rec_len;
+                        dp = (DIR *) cp; //shut up
+                }
+                //printf("finished printDir()\n");
+        }
+        else
+        {
+                //printf("not a directory!\n");
+        }
+}
