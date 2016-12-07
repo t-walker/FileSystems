ea3152f9ca99aea52e6d694ae81cf716eef63b7b
Tyler Walker <twalker@eecs.wsu.edu>
2016-12-07 00:05:35 -0800

    adding all the files to make it work

--- operations/read_write.c
+++ operations/read_write.c
@@ -2,86 +2,86 @@
 
 int my_read(int fd, char buffer[], int count)
 {
-  u32  d_buf[256];
-  u32 di_buf[256];
+        u32 d_buf[256];
+        u32 di_buf[256];
 
-  char read_buffer[BLOCK_SIZE];
+        char read_buffer[BLOCK_SIZE];
 
-  OFT *oftEntry = running->fd[fd];
-  MINODE *mip = oftEntry->mptr;
+        OFT *oftEntry = running->fd[fd];
+        MINODE *mip = oftEntry->mptr;
 
-  int count = 0;
-  int read_in = 0;
-  int remain;
+        int r_count = 0;
+        int read_in = 0;
+        int remain;
 
-  int avail = mip->INODE.i_size - oftEntry->offset;
-  char *cp = buffer;
-  char *cr;
+        int avail = mip->INODE.i_size - oftEntry->offset;
+        char *cp = buffer;
+        char *cr;
 
-  int blk;
-  int lbk;
-  int i_blk;
-  int start_byte;
+        int blk;
+        int lbk;
+        int i_blk;
+        int start_byte;
 
-  while(count && avail)
-  {
-    cp = buffer;
+        while(count && avail)
+        {
+                cp = buffer;
 
-    lbk = oftEntry->offset / BLOCK_SIZE;
-    start_byte = oftEntry->offset % BLOCK_SIZE;
+                lbk = oftEntry->offset / BLOCK_SIZE;
+                start_byte = oftEntry->offset % BLOCK_SIZE;
 
-    if (lbk < 12)
-    {
-      blk = mip->INODE.i_block[lbk];
-    }
-    else if (lbk >= 12 && lbk < 256 + 12)
-    {
-      get_block(mip->dev, mip->INODE.i_block[12], (char *)d_buf);
-      blk = d_buf[lbk - 12];
-    }
-    else
-    {
-      get_block(mip->dev, mip->INODE.i_block[13], (char *)d_buf);
-      i_blk = (lbk - (256 + 12)) / 256;
-      get_block(mip->dev, d_buf[i_blk], (char *)di_buf);
-      blk = di_buf[(lbk - (256 + 12)) % 256];
-    }
+                if (lbk < 12)
+                {
+                        blk = mip->INODE.i_block[lbk];
+                }
+                else if (lbk >= 12 && lbk < 256 + 12)
+                {
+                        get_block(mip->dev, mip->INODE.i_block[12], (char *)d_buf);
+                        blk = d_buf[lbk - 12];
+                }
+                else
+                {
+                        get_block(mip->dev, mip->INODE.i_block[13], (char *)d_buf);
+                        i_blk = (lbk - (256 + 12)) / 256;
+                        get_block(mip->dev, d_buf[i_blk], (char *)di_buf);
+                        blk = di_buf[(lbk - (256 + 12)) % 256];
+                }
 
-    get_block(mip->dev, blk, read_buffer);
+                get_block(mip->dev, blk, read_buffer);
 
-    cr = buf + start_byte;
+                cr = buf + start_byte;
 
-    remain = BLOCK_SIZE - start_byte;
+                remain = BLOCK_SIZE - start_byte;
 
-    while (remain > 0)
-    {
-      if (avail <= remain)
-      {
-        read_in = avail;
-      }
-      else
-      {
-        read_in = remain;
-      }
+                while (remain > 0)
+                {
+                        if (avail <= remain)
+                        {
+                                read_in = avail;
+                        }
+                        else
+                        {
+                                read_in = remain;
+                        }
 
-      memcpy(cr, cp, read_in);
+                        memcpy(cr, cp, read_in);
 
-      oftEntry->offset += read_in;
+                        oftEntry->offset += read_in;
 
-      count += read_in;
+                        r_count += read_in;
 
-      avail -= read_in;
+                        avail -= read_in;
 
-      count -= read_in;
+                        count -= read_in;
 
-      remain -= read_in;
+                        remain -= read_in;
 
-      if (count <= 0 || avail <= 0)
-      {
-        break;
-      }
-    }
-  }
+                        if (count <= 0 || avail <= 0)
+                        {
+                                break;
+                        }
+                }
+        }
 
-  return count;
+        return r_count;
 }
