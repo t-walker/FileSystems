aab688f56d2bb4d78cb3826a48cc524b539b8bfc
Tyler Walker <twalker@eecs.wsu.edu>
2016-12-06 19:27:18 -0800

    Beautifying file

--- operations/stat.c
+++ operations/stat.c
@@ -2,7 +2,7 @@
 
 void my_stat(char *pathname)
 {
-        printf("cat() ---------------------\n");
+        //printf("stat() ---------------------\n");
         MINODE *mip;
 
         int ino = running->cwd->ino;
@@ -10,17 +10,18 @@ void my_stat(char *pathname)
 
         ino = getino(pathname, dev);
 
-
-        if (!ino)
+        if (ino)
         {
-                return;
+                mip = iget(dev, ino);
+
+                printf("device: %4d  indode:  %4d\n", dev, ino);
+                printf("size:   %4d  nlink:   %4d\n", mip->INODE.i_size, mip->INODE.i_links_count);
+                printf("file mode: %o  gid: %d uid: %d\n", mip->INODE.i_mode, mip->INODE.i_gid, mip->INODE.i_uid);
+
+                iput(mip);
         }
-
-        mip = iget(dev, ino);
-
-        printf("device: %4d  indode:  %4d\n", dev, ino);
-        printf("size:   %4d  nlink:   %4d\n", mip->INODE.i_size, mip->INODE.i_links_count);
-        printf("file mode: %o  gid: %d uid: %d\n", mip->INODE.i_mode, mip->INODE.i_gid, mip->INODE.i_uid);
-
-        iput(mip);
+        else
+        {
+                printf("stat() -- No file!\n");
+        }
 }
