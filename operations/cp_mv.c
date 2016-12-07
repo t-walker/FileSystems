6867c1f938df32060b9647458a43e9562528425a
Micah <micah@debian>
2016-12-07 06:48:49 -0800

    cp and mv work! I think

--- operations/cp_mv.c
+++ operations/cp_mv.c
@@ -38,30 +38,26 @@ mv src dest:
 void my_cp(char *old_file, char *new_file)
 {
   char mybuf[BLKSIZE];
+  char *strs[3] = {"placeholder","777", new_file};
   //open old for read
   int fd1 = my_open(old_file, O_RDONLY);
 
   //make sure we create or already have new_file in the system
   int fd2 = my_open(new_file, O_CREAT);
   my_close(fd2);
+  ch_mod(strs, 3);
   //open new for write
   fd2 = my_open(new_file, O_WRONLY);
 
   //write from old to new
   int r;
-  while (r = my_read(fd1, mybuf, BLKSIZE))
+  while (r)
   {
-    //last bit of data
-    if (r < BLKSIZE)
-      break;
+    r = my_read(fd1, mybuf, BLKSIZE);
     //write the data
     my_write(fd2, mybuf, r);
     memset(mybuf, 0, BLKSIZE);//clear out the buffer
   }
-
-  //write the rest of the data
-  my_write(fd2, mybuf, r);
-
   //close the files
   my_close(fd1);
   my_close(fd2);
@@ -69,6 +65,21 @@ void my_cp(char *old_file, char *new_file)
 
 void my_mv(char *old_file, char *new_file)
 {
-  my_cp(old_file, new_file);
-  my_rm(old_file);
+  //1. verify src exists; get its INODE in ==> you already know its dev
+  int ino = getino(old_file, running->cwd->dev);
+  if(!ino)
+  {
+    return;
+  }
+  //2. check whether src is on the same dev as dest
+
+              //  CASE 1: same dev:
+  //3. Hard link dst with src (i.e. same INODE number)
+  my_link (old_file, new_file);
+  //4. unlink src (i.e. rm src name from its parent directory and reduce INODE's link count by 1).
+  my_unlink(old_file);
+    //            CASE 2: not the same dev:
+  //3. cp src to dst
+//  4. unlink src
+//  my_cp(old_file, new_file);
 }
